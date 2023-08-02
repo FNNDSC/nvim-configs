a1d56486966b8b48ba26c635aae646fbc7e8bbe2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-08-02 16:00:01 -0400

    Initial commit

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,91 @@
+require("mason-lspconfig").setup()
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require('lspsaga').setup({
+  code_action_icon = "💡",
+  symbol_in_winbar = {
+    in_custom = false,
+    enable = true,
+    separator = ' ',
+    show_file = true,
+    file_formatter = ""
+  },
+})
+
+vim.keymap.set("n", "gd", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', { silent = true })
+vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
+
+require("lspconfig").lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+    },
+  }
+}
+
+require('lspconfig').pyright.setup{}
+
+require('lspsaga').setup {
+  -- Your existing lspsaga configuration options here
+
+  -- Enable the inline documentation feature
+  code_action_icon = '',  -- Set the icon for code actions
+  code_action_prompt = {
+    enable = true,  -- Enable the code action prompt
+  },
+  code_action_keys = {
+    quit = '<ESC>',  -- Set the key to quit the code action prompt
+    exec = '<CR>',   -- Set the key to execute the code action
+  },
+}
+
+require("lspconfig").solargraph.setup {
+  capabilities = capabilities,
+}
+
+require("lspconfig").pyright.setup {
+  capabilities = capabilities,
+}
+
+-- disable virtual_text (inline) diagnostics and use floating window
+-- format the message such that it shows source, message and
+-- the error code. Show the message with <space>e
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	float = {
+		border = "single",
+		format = function(diagnostic)
+			return string.format(
+				"%s (%s) [%s]",
+				diagnostic.message,
+				diagnostic.source,
+				diagnostic.code or diagnostic.user_data.lsp.code
+			)
+		end,
+	},
+})
+
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+-- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
+-- vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<space>e', ':Lspsaga show_line_diagnostics<CR>', { noremap = true, silent = true })
+
+
