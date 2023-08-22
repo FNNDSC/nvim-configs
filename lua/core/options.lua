14d81351dc49a7c42b868eb13b1b7cb5b01470a9
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-08-22 12:51:40 -0400

    Set doge bindings to default

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,8 +9,8 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 
 -- use spaces for tabs and whatnot
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
@@ -18,4 +18,7 @@ vim.cmd [[ set noswapfile ]]
 
 --Line numbers
 vim.wo.number = true
+
+vim.g.doge_enable_mappings = 1  -- Set the value to 1 to enable mappings
+
 
