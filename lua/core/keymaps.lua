344f37d215604e720c098eec4e86d20340f540ba
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-12-18 14:58:00 -0500

    Add undo <leader>u

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>u', ':undo<CR>')
 
 -- Define the keybinding to append a specific string to the current line
 vim.api.nvim_set_keymap('n', '<Leader>z', [[:lua vim.fn.append('.', ',rudolph.pienaar@childrens.harvard.edu')<CR>]], { noremap = true, silent = true })
